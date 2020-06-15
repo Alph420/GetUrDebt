aa750992da823625f24c104625d5f8a03e140f38
Alph420 <julienjennequin59123@gmail.com>
2020-06-15 16:13:57 +0200

    add firebase

--- app/src/main/java/com/alphacorporations/givememymoney/View/AddDebtActivity.java
+++ app/src/main/java/com/alphacorporations/givememymoney/View/AddDebtActivity.java
@@ -143,6 +143,7 @@ public class AddDebtActivity extends AppCompatActivity {
 
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
