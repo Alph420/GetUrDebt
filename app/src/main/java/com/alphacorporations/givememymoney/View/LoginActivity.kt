19748ec78f4a3cf0a03ada2a4828252dc652c79f
Alph420 <julienjennequin59123@gmail.com>
2020-07-22 19:22:45 +0200

    Fix loginLayout correct some bug in Sign up layout

--- app/src/main/java/com/alphacorporations/givememymoney/View/LoginActivity.kt
+++ app/src/main/java/com/alphacorporations/givememymoney/View/LoginActivity.kt
@@ -1,13 +1,12 @@
 package com.alphacorporations.givememymoney.View
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.alphacorporations.givememymoney.R
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
 
 /**
@@ -23,46 +22,42 @@ class LoginActivity : AppCompatActivity() {
 
         val intent = Intent(this, ListDebtActivity::class.java)
 
-        //if(email.isEmpty() || password.isEmpty()){ btn_sign_up.Ac}
-
 
         //Click sur le btn login
-        bottom_login.setOnClickListener {
+        btn_sign_in.setOnClickListener {
             val email = email_edit_text.text.toString()
             val password = password_edit_text.text.toString()
 
-            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this) { task ->
-                        //Si connexion est bonne start l'app
-                        if (task.isSuccessful) {
-                            startActivity(intent)
-                        }
-                        //Sinon retour a l'ecran d'enregistrement
-                        else {
-                            error_msg.visibility = View.VISIBLE
-                        }
-                    }
+            if(email.isEmpty() || password.isEmpty()){
+                error_msg.text = if(email.isEmpty()) "Veuillez rentrer une adresse email" else "Veuillez rentrer un mot de passe"
+                error_msg.visibility = View.VISIBLE
+                btn_sign_in.highlightColor = Color.RED
+            }
+            else{
+                error_msg.visibility = View.INVISIBLE
+                connexion(email,password)
+            }
         }
 
-        btn_sign_up.setOnClickListener {
-            val email = email_edit_text.text.toString()
-            val password = password_edit_text.text.toString()
-
-
-
-            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            // Sign in success, update UI with the signed-in user's information
-                            println("createUserWithEmail:success")
-                            startActivity(intent)
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            println("createUserWithEmail:failure")
-                            Toast.makeText(baseContext, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show()
-                        }
-                    }
+        btn_sign_up_activity.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
         }
+    }
+
+
+    private fun connexion(email: String, password: String) {
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    //Si connexion est bonne start l'app
+                    if (task.isSuccessful) {
+                        startActivity(intent)
+                    }
+                    //Sinon retour a l'ecran d'enregistrement
+                    else {
+                        error_msg.text = getString(R.string.error_login)
+                        error_msg.visibility = View.VISIBLE
+                    }
+                }
     }
 }
---
+++ app/src/main/java/com/alphacorporations/givememymoney/View/SignUpActivity.kt
@@ -0,0 +1,65 @@
+package com.alphacorporations.givememymoney.View
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.alphacorporations.givememymoney.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.email_edit_text
+import kotlinx.android.synthetic.main.activity_login.password_edit_text
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import kotlinx.android.synthetic.main.activity_sign_up.error_msg
+
+/**
+Created by Alph4 le 22/07/2020.
+Projet: Give Me My Money
+ **/
+class SignUpActivity : AppCompatActivity() {
+
+    var valide = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        val intent = Intent(this, ListDebtActivity::class.java)
+
+
+        //Click sur le btn sign up
+        btn_sign_up.setOnClickListener {
+            val pseudo = peusdo_edit_text.text.toString()
+            val email = email_edit_text.text.toString()
+            val password = password_edit_text.text.toString()
+            val passwordConfirmation = password_edit_text_assurance.text.toString()
+
+            verification()
+
+            if(valide) signUp(email, password)
+            //TODO else msg erreur
+
+
+        }
+    }
+
+    //Verification if existing user with the same email or pseudo existing in databse
+    fun verification() {
+    }
+
+
+    fun signUp(email: String, password: String) {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        println("createUserWithEmail:success")
+                        startActivity(intent)
+                    } else {
+                        error_msg.text = getString(R.string.error_sign_up)
+                        error_msg.visibility = View.VISIBLE
+                    }
+                }
+    }
+
+}
