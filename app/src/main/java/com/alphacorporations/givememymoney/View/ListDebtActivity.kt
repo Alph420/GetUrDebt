74e2dfb7d0e6024764dc3525f6caf3f7b0ddda9a
Alpha <julienjennequin59123@gmail.com>
2020-07-24 12:25:27 +0200

    edit AddDebtActivity.kt

--- app/src/main/java/com/alphacorporations/givememymoney/View/ListDebtActivity.kt
+++ app/src/main/java/com/alphacorporations/givememymoney/View/ListDebtActivity.kt
@@ -71,7 +71,7 @@ class ListDebtActivity : AppCompatActivity() {
             }
 
             debtList = listDebtFromFirebase
-            if(debtList.isEmpty()) progressBar_loading_debt.visibility = View.VISIBLE
+            if(debtList.isEmpty()) progressBar_loading_debt.visibility = View.GONE
             setAndUseAdapter()
         }
                 .addOnFailureListener { exception ->
